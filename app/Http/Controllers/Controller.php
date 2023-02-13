a7a49ed0370d6de958b58764c49a8a6f641066a8
agusmichaelsianipar <agus010sianipar@gmail.com>
2023-02-13 13:04:40 +0700

    answer of number 1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,66 @@
-<?php
-
-namespace App\Http\Controllers;
+<?php namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+class Controller extends BaseController {
+    use AuthorizesRequests,
+    DispatchesJobs,
+    ValidatesRequests;
+
+    public function answer1() {
+
+        $words=array("kita", "atik", "tika", "aku", "kia", "makan", "kua");
+        $result=array();
+        $dumpArr=array();
+
+        for($i=0; $i<count($words); $i++) {
+            $anagramArr=array();
+
+            if(in_array($words[$i], $dumpArr)) {
+                continue;
+            }
+
+            for($j=0; $j<count($words); $j++) {
+                if($this->isAnagram($words[$i], $words[$j])===1) {
+                    $anagramArr[]=$words[$j];
+                    $dumpArr[]=$words[$j];
+                }
+            }
+
+            $result[]=$anagramArr;
+        }
+
+        echo json_encode($result);
+    }
+
+    public function isAnagram($str1, $str2) {
+        $char1=str_split($str1);
+        $char2=str_split($str2);
+        $status=0;
+
+        if(count($char1) !=count($char2)) {
+            return $status;
+        }
+
+        sort($char1);
+        sort($char2);
+
+        for ($i=0; $i<count($char1); $i++) {
+            if ($char1[$i] !==$char2[$i]) {
+                $status=0;
+                break;
+            }
+
+            else {
+                if($i==count($char1)-1) {
+                    $status=1;
+                }
+            }
+        }
+
+        return $status;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo.php', function () {
+    return public_path('answer/no1.php');
+});
+
+Route::get('/answer1', [Controller::class, 'answer1']);
